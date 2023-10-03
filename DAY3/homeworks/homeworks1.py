9c35db6a4fd8c2df27ba2b4ae20f6b93090d4719
Zuza <144389463+T-zuza@users.noreply.github.com>
2023-10-03 20:52:57 +0400

    Add files via upload

---
+++ DAY3/homeworks/homeworks1.py
@@ -0,0 +1,9 @@
+name=input("enter your name: ")
+surname=input("enter your surname: ")
+age= input("enter your age: ")
+print( "my name is "  +  name  + "my surname is "  + surname   + "my age is "   +  age )
+
+# age After 25 years
+
+age= input("enter your age: ")
+print("After 25 years you will be ", int(age)  +  25 , "years old" )
