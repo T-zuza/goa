13b923c46417bdf18c59103766587b3887ed26b1
Zuza <144389463+T-zuza@users.noreply.github.com>
2023-10-15 00:33:53 +0400

    Add files via upload

---
+++ DAY7/classworks/classwork1.py
@@ -0,0 +1,31 @@
+my_list = ["xinkali", "mwvadi","vashli", 7, 2.5]
+print(my_list)
+
+name = "Sarah"
+age = 34
+country = "Germany"
+info = [name, age, country]
+print(info[0])
+print(info[1])
+print(info[2])
+
+products = ["juice", "chocolate", "water"]
+user_choice = 1
+print(products[user_choice])
+
+
+#Indexing also works with strings
+animal = "Dog"
+print(animal[0])
+print(animal[1])
+print(animal[2])
+
+notification = "New message!"
+print(notification[4])
+
+x = "arctic"
+print(x[2] + x[0] + x[3])
+
+# Slicing
+animals =["cat", "dog", "bird", "cow"]
+print(animals[1:3])
